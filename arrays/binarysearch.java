c9e7b32d38a96fc61ac707705997636d76648a50
tusharx0809 <tusharbhatia0809@outlook.com>
2025-10-25 18:55:21 +0530

    added binary search on arrays

--- arrays/binarysearch.java
+++ arrays/binarysearch.java
@@ -1,22 +1,28 @@
 package arrays;
+public class Binarysearch {
+    public static int binarysearch(int[] arr, int num){
+        int l = 0;
+        int r = arr.length - 1;
 
-public class LinearSearch {
-    public static int linearSearch(int[] arr, int num) {
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == num) {
-                return i;  
+        while(l <= r){
+            int m = l + (r-l) / 2;
+            if(arr[m] == num){
+                return m;
+            }else if(arr[m] < num){
+                l = m+1;
+            }else{
+                r = m-1;
             }
         }
-        return -1; 
+        return -1;
     }
-
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
         int searchNum1 = 3;
         int searchNum2 = 6;
 
-        int result1 = linearSearch(arr, searchNum1);
-        int result2 = linearSearch(arr, searchNum2);
+        int result1 = binarysearch(arr, searchNum1);
+        int result2 = binarysearch(arr, searchNum2);
 
         System.out.println("Search for " + searchNum1 + ": " + (result1 != -1 ? "Found at index " + result1 : "Not found")); //Search for 3: Found at index 2
         System.out.println("Search for " + searchNum2 + ": " + (result2 != -1 ? "Found at index " + result2 : "Not found")); //Search for 6: Not found
